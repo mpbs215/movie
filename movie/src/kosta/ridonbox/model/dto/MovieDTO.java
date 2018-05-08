b5a5c69ba6b742cbe89113d8a8c03f27eafe3e79
KOSTA4 <KOSTA4@192.168.0.145>
2018-05-08 12:48:34 +0900

    movieDTO수정

--- movie/src/kosta/ridonbox/model/dto/MovieDTO.java
+++ movie/src/kosta/ridonbox/model/dto/MovieDTO.java
@@ -1,84 +1,117 @@
 package kosta.ridonbox.model.dto;
 
 public class MovieDTO {
-	private String movieNo;		//pk
-	private String title;		//제목
-	private String titleEnglish;//영어제목
-	private String director;	//감독
-	private String actor;		//배우
-	private String releasedate;	//개봉일
-	private String imgPath;			//이미지경로
-	private int grade;				//평점
-	private int state;				//영화 상태(상영중 1, 상영종료 0)
+	private String movie_num;		//pk
+	private String movie_title;		//제목
+	private String movie_etitle;//영어제목
+	private String movie_dir;	//감독
+	private String movie_act;		//배우
+	private String movie_date;	//개봉일
+	private int movie_rat;				//평점
+	private String movie_path;			//이미지경로
+	private String movie_youtube;		//유튜브경로
+	private int movie_state;				//영화 상태(상영중 1, 상영종료 0)
 	
 	public MovieDTO() {
 	}
-	public MovieDTO(String movieNo, String title, String titleEnglish, String director, String actor, String releasedate,
-			String imgPath, int grade, int state) {
-		this.movieNo = movieNo;
-		this.title = title;
-		this.titleEnglish = titleEnglish;
-		this.director = director;
-		this.actor = actor;
-		this.releasedate = releasedate;
-		this.imgPath = imgPath;
-		this.grade = grade;
-		this.state = state;
+
+	public MovieDTO(String movie_num, String movie_title, String movie_etitle, String movie_dir, String movie_act,
+			String movie_date, int movie_rat, String movie_path, String movie_youtube, int movie_state) {
+		super();
+		this.movie_num = movie_num;
+		this.movie_title = movie_title;
+		this.movie_etitle = movie_etitle;
+		this.movie_dir = movie_dir;
+		this.movie_act = movie_act;
+		this.movie_date = movie_date;
+		this.movie_rat = movie_rat;
+		this.movie_path = movie_path;
+		this.movie_youtube = movie_youtube;
+		this.movie_state = movie_state;
 	}
-	public String getMovieNo() {
-		return movieNo;
+
+	public String getMovie_num() {
+		return movie_num;
 	}
-	public void setMovieNo(String movieNo) {
-		this.movieNo = movieNo;
+
+	public void setMovie_num(String movie_num) {
+		this.movie_num = movie_num;
 	}
-	public String getTitle() {
-		return title;
+
+	public String getMovie_title() {
+		return movie_title;
 	}
-	public void setTitle(String title) {
-		this.title = title;
+
+	public void setMovie_title(String movie_title) {
+		this.movie_title = movie_title;
 	}
-	public String getTitleEnglish() {
-		return titleEnglish;
+
+	public String getMovie_etitle() {
+		return movie_etitle;
 	}
-	public void setTitleEnglish(String titleEnglish) {
-		this.titleEnglish = titleEnglish;
+
+	public void setMovie_etitle(String movie_etitle) {
+		this.movie_etitle = movie_etitle;
 	}
-	public String getDirector() {
-		return director;
+
+	public String getMovie_dir() {
+		return movie_dir;
 	}
-	public void setDirector(String director) {
-		this.director = director;
+
+	public void setMovie_dir(String movie_dir) {
+		this.movie_dir = movie_dir;
 	}
-	public String getActor() {
-		return actor;
+
+	public String getMovie_act() {
+		return movie_act;
 	}
-	public void setActor(String actor) {
-		this.actor = actor;
+
+	public void setMovie_act(String movie_act) {
+		this.movie_act = movie_act;
 	}
-	public String getReleasedate() {
-		return releasedate;
+
+	public String getMovie_date() {
+		return movie_date;
 	}
-	public void setReleasedate(String releasedate) {
-		this.releasedate = releasedate;
+
+	public void setMovie_date(String movie_date) {
+		this.movie_date = movie_date;
 	}
-	public String getImgPath() {
-		return imgPath;
+
+	public int getMovie_rat() {
+		return movie_rat;
 	}
-	public void setImgPath(String imgPath) {
-		this.imgPath = imgPath;
+
+	public void setMovie_rat(int movie_rat) {
+		this.movie_rat = movie_rat;
 	}
-	public int getGrade() {
-		return grade;
+
+	public String getMovie_path() {
+		return movie_path;
 	}
-	public void setGrade(int grade) {
-		this.grade = grade;
+
+	public void setMovie_path(String movie_path) {
+		this.movie_path = movie_path;
 	}
-	public int getState() {
-		return state;
+
+	public String getMovie_youtube() {
+		return movie_youtube;
 	}
-	public void setState(int state) {
-		this.state = state;
+
+	public void setMovie_youtube(String movie_youtube) {
+		this.movie_youtube = movie_youtube;
 	}
+
+	public int getMovie_state() {
+		return movie_state;
+	}
+
+	public void setMovie_state(int movie_state) {
+		this.movie_state = movie_state;
+	}
+	
+	
+	
 	
 	
 	
